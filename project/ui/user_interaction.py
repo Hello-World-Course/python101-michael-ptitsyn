3ad705d62400dbc185766f3b243d642affa411e0
michael-ptitsyn <ptitsyn@post.bgu.ac.il>
2024-04-29 23:41:33 +0300

    done

---
+++ project/ui/user_interaction.py
@@ -0,0 +1,4 @@
+name = input("Hello, whats your name")
+board_size = input("please choose board size:")
+number_of_mins = input("Michael, for board size 9, choose number of mines to allocate:")
+print("Michael, the board size is: 9, number of mines is: 7. ENJOY!")
