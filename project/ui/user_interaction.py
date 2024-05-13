bcdcf0ef9621b66ea32f691e3e4ab3d7ec9f616d
michael-ptitsyn <ptitsyn@post.bgu.ac.il>
2024-05-13 03:26:15 +0300

    done

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -6,4 +6,4 @@ def is_board_size_valid(b_s):
     return 0 < b_s < 26
 
 def is_number_of_mines_valid(board_size, number_of_mines):
-    return number_of_mines < 0.5 * board_size*board_size
+    return number_of_mines and number_of_mines < 0.5 * board_size*board_size
